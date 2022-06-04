8c41a7b6ed90c1278bc7cb095ab06930abf3b0c1
nathanjpak <77936853+nathanjpak@users.noreply.github.com>
2022-06-03 17:52:37 -0700

    Update submission.js
    complete first and third challenges

--- submission.js
+++ submission.js
@@ -1,5 +1,11 @@
 const findSum = function(array) {
     // your code here - don't forget to return a number!
+    //loop through the numbers in the array and add them up
+    let num = 0;
+    for (i=0; i < array.length; i++) {
+        num = num + array[i];
+    }
+    return num
   };
   
   const findFrequency = function(array) {
@@ -8,6 +14,28 @@ const findSum = function(array) {
   
   const isPalindrome = function(str) {
     // your code here - don't forget to return a boolean!
+    //make string lowercase to get things uniform
+    str = str.toLowerCase();
+    //get length of string
+    let length = str.length;
+    //divide length in half and floor to account for odd numbers
+    length = Math.floor(length / 2);
+    //create string for first half using slice
+    let firstHalf = str.slice(0,length);
+    //create string for second half using substr
+    let secondHalf = str.substr(-length);
+    let dummyVar = 0;
+    //cross reference strings using for loop with charAt
+    for (i = 0; i < length; i++) {
+        (firstHalf.charAt(i) !== secondHalf.charAt(length-1-i)) ? dummyVar = dummyVar+1 : dummyVar = dummyVar;
+    }
+    //if dummy var is 0 return true, else return false
+    if (dummyVar === 0) {
+        return true;
+    }
+    else {
+        return false;
+    }
   };
   
   const largestPair = function(array) {
